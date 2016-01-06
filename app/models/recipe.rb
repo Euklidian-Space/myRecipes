5c8c7fb400d08ed52d7b9d0b29fbaaadea9aa48c
euklidian_space <gabriel.grover@gmail.com>
2016-01-06 02:03:21 +0000

    added tests for recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,11 @@
 class Recipe < ActiveRecord::Base
-  
+  validates :name, presence: true,
+                   length: {maximum:100,minimum:5}
+                   
+  validates :summary, presence: true,
+                      length: {minimum:10, maximum:150}
+                      
+  validates :description, presence: true,
+                          length: {minimum: 20, maximum:500}
+              
 end
